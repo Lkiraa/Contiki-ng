5b107ebcf7399b748111d0a22dc3a3801da29908
Lkiraa <aboubacarsougoule39@gmail.com>
2022-09-24 10:48:11 +0100

    update udp-client

--- examples/project/udp-client.c
+++ examples/project/udp-client.c
@@ -8,11 +8,13 @@
 #include "net/ipv6/simple-udp.h"
 #include "net/ipv6/uip-ds6-nbr.h"
 #include "net/ipv6/uip-icmp6.h"
-
-
+#include "sys/energest.h"
+#include "services/simple-energest/simple-energest.h"
+#include "hack.h"
 
 #include <stdlib.h>
 #include <stdio.h>
+#include "attributes.h"
 
 #include "sys/log.h"
 #define LOG_MODULE "App"
@@ -30,9 +32,10 @@ uip_ds6_nbr_t *uip_ds6_nbr_next(uip_ds6_nbr_t *nbr);
  rpl_rank_t increase_rank(rpl_rank_t base_rank);
  rpl_rank_t my_rank(void);
  uip_ipaddr_t *rpl_parent_get_ipaddr(rpl_parent_t *nbr);
- static const char *rpl_mop_to_str(int mop);
  static const char *rpl_ocp_to_str(int ocp);
 /*-----------------------------------variables-----------------------------------*/
+static int ptosend = 0;
+  static int ptorecev = 0;
 
 static struct simple_udp_connection udp_conn;
  rpl_instance_t *current_instance ;
@@ -51,7 +54,7 @@ udp_rx_callback(struct simple_udp_connection *c,
          const uint8_t *data,
          uint16_t datalen)
 {
-
+ptorecev++;
   LOG_INFO("Received response '%.*s' from ", datalen, (char *) data);
   LOG_INFO_6ADDR(sender_addr);
 #if LLSEC802154_CONF_ENABLED
@@ -61,42 +64,33 @@ udp_rx_callback(struct simple_udp_connection *c,
 
 }
 
-/*rpl mode operation*/
-static const char *rpl_mop_to_str(int mop)
-{
-  switch(mop) {
-    case RPL_MOP_NO_DOWNWARD_ROUTES:
-      return "No downward routes";
-    case RPL_MOP_NON_STORING:
-      return "Non-storing";
-    case RPL_MOP_STORING_NO_MULTICAST:
-      return "Storing";
-    case RPL_MOP_STORING_MULTICAST:
-      return "Storing+multicast";
-    default:
-      return "Unknown";
-  }
-}
 
 /*list of neighbord*/
  void neighbor_list()
 {
-  
   if(!current_instance->used) {
     LOG_INFO("-- Instance: None\n");
   }else{
-    LOG_INFO("-- MOP: %s\n", rpl_mop_to_str(current_instance->mop));
-    LOG_INFO("-- OF: %s\n", rpl_ocp_to_str(current_instance->of->ocp));
-    LOG_INFO("-- legitiate Rank: %u\n", legitimate_rank);
-    LOG_INFO("-- illegitiate Rank: %u\n", illegitimate_rank);
-     LOG_INFO("--preferred parent: ");
+    LOG_INFO(" -- version: %u -- OF: %s -- Rank: %u -- illegitiate_Rank: %u -- pref_parent: "
+    ,current_instance->current_dag->version,rpl_ocp_to_str(current_instance->of->ocp),legitimate_rank, illegitimate_rank);
     LOG_INFO_6ADDR(rpl_parent_get_ipaddr(current_instance->current_dag->preferred_parent));
-    LOG_INFO("\nListe des voisins:\n");
+    printf(" -- Trickle_timer: current %u min %u max %u DIO_INIT %u DIO_OUT %u DIO_IN %u",
+      current_instance->dio_intcurrent, current_instance->dio_intmin,
+      (current_instance->dio_intmin + current_instance->dio_intdoubl), current_instance->dio_totint, current_instance->dio_totsend, current_instance->dio_totrecv);
+   printf(" DIS_OUT: %u DIS_IN: %u DAO_OUT: %u DAO_IN: %u",dis_send, dis_recv, dao_send, dao_recv);
+    
+/*    simple_energest_step();
+*/    printf(" -- Period_summary 0 ( 60 seconds) -- Energie: %10lu -- Puissance %10lu",e, p);
+    printf(" -- DTSN_out: %u -- data_sent: %d -- data_receved: %d", current_instance->dtsn_out, ptosend, ptorecev);
+     printf(" Liste_des_voisins: ");
     uip_ds6_nbr_t *nbr_l;
     for(nbr_l = uip_ds6_nbr_head(); nbr_l != NULL; nbr_l = uip_ds6_nbr_next(nbr_l)) {
       LOG_INFO_6ADDR(&nbr_l->ipaddr);
-      printf("\n");
+      if(uip_ds6_nbr_next(nbr_l) != NULL){printf("|");}
+      
     }
+    
+    printf("\n");
     free_neighbor_list();
   }
   
@@ -141,70 +135,7 @@ static const char *rpl_ocp_to_str(int ocp)
   }
 }
 
- /*void status(){
-  // status rpl
-
-  LOG_INFO("RPL status:\n");
-  if(!current_instance->used) {
-    LOG_INFO("-- Instance: None\n");
-  } else {
-    LOG_INFO("-- Instance: %u\n", current_instance->instance_id);
-    if(NETSTACK_ROUTING.node_is_root()) {
-      LOG_INFO("-- DAG root\n");
-    } else {
-      LOG_INFO("-- DAG node\n");
-    }
-    LOG_INFO("-- DAG: ");
-    LOG_INFO_6ADDR(&current_instance->current_dag->dag_id);
-    LOG_INFO("\n, version %u\n", current_instance->current_dag->version);
-    LOG_INFO("-- Prefix: ");
-    LOG_INFO_6ADDR(&current_instance->current_dag->prefix_info.prefix);
-    LOG_INFO("/%u\n", current_instance->current_dag->prefix_info.length);
-    LOG_INFO("-- MOP: %s\n", rpl_mop_to_str(current_instance->mop));
-    LOG_INFO("-- OF: %s\n", rpl_ocp_to_str(current_instance->of->ocp));
-    LOG_INFO("-- Hop rank increment: %u\n", current_instance->min_hoprankinc);
-    LOG_INFO("-- Default lifetime: %u seconds\n", current_instance->lifetime_unit);
-
-    //LOG_INFO("-- State: %s\n", rpl_state_to_str(current_instance.current_dag->state));
-    LOG_INFO("-- Preferred parent: ");
-    if(current_instance->current_dag->preferred_parent) {
-      LOG_INFO_6ADDR(rpl_parent_get_ipaddr(current_instance->current_dag->preferred_parent));
-      LOG_INFO(" (last DTSN: %u)\n", current_instance->current_dag->preferred_parent->dtsn);
-    } else {
-      LOG_INFO("None\n");
-    }
-   LOG_INFO("-- Rank: %u\n", current_instance->current_dag->rank);
-   LOG_INFO("-- liste of neighboard: \n");
-    LOG_INFO("-- Lowest rank: %u (%u)\n", current_instance.current_dag->lowest_rank, current_instance.max_rankinc);
-    LOG_INFO("-- DTSN out: %u\n", current_instance.dtsn_out);
-    LOG_INFO("-- DAO sequence: last sent %u, last acked %u\n",
-        current_instance.current_dag->dao_last_seqno, current_instance.current_dag->dao_last_acked_seqno);
-    LOG_INFO("-- Trickle timer: current %u, min %u, max %u, redundancy %u\n",
-      current_instance.current_dag->dio_intcurrent, current_instance.dio_intmin,
-      current_instance.dio_intmin + current_instance.dio_intdoubl, current_instance.dio_redundancy);
-
-  }
-}*/
-
-
-/*frrrrrrrrrrrrrrrrrrrrrrrrrr*/
-
-/*static const char *rpl_state_to_str(enum rpl_dag_state state)
-{
-  switch(state) {
-    case DAG_INITIALIZED:
-      return "Initialized";
-    case DAG_JOINED:
-      return "Joined";
-    case DAG_REACHABLE:
-      return "Reachable";
-    case DAG_POISONING:
-      return "Poisoning";
-    default:
-      return "Unknown";
-  }
-}*/
-
+ 
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(udp_client_process, ev, data)
 {
@@ -215,6 +146,7 @@ PROCESS_THREAD(udp_client_process, ev, data)
   current_instance = rpl_get_default_instance();
   legitimate_rank = my_rank();
   illegitimate_rank = increase_rank(legitimate_rank);
+  set_hack(-1);
   PROCESS_BEGIN();
   /* Initialize UDP connection */
   simple_udp_register(&udp_conn, UDP_CLIENT_PORT, NULL,
@@ -232,6 +164,7 @@ PROCESS_THREAD(udp_client_process, ev, data)
       snprintf(str, sizeof(str), "hello %d", count);
       simple_udp_sendto(&udp_conn, str, strlen(str), &dest_ipaddr);
       count++;
+      ptosend = (int)count;
       // sending diio message
       neighbor_list();
 
